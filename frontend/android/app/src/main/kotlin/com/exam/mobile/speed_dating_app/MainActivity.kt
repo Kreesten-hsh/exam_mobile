ce5320939c72a703492a5989d456668818e463d8
Kreesten-hsh <akreesten@gmail.com>
2026-02-08 00:00:15 +0100

    chore: remove an unspecified file.

--- frontend/android/app/src/main/kotlin/com/exam/mobile/speed_dating_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.exam.mobile.speed_dating_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/domain/repositories/participant_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/participant_entity.dart';
-
-abstract class ParticipantRepository {
-  Future<List<ParticipantEntity>> getParticipants(String eventId);
-  Future<void> cancelParticipation(String participantId);
-}
